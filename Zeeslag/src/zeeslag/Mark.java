dcc3eb8f931653b1c93ab3e084d7ef9d324d0cd5
jonas2211 <jonas.iven@student.uhasselt.be>
2019-01-11 00:49:41 +0100

    klasse mark

---
+++ Zeeslag/src/zeeslag/Mark.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package zeeslag;
+
+/**
+ *
+ * @author Bavo
+ */
+public class Mark {
+    private boolean hit;
+    private Boot boot;
+    private int x,y;
+// x en y coordinaten zijn vaag
+    public Mark(int x, int y) {
+        hit = true;
+        this.x = x;
+        this.y = y;
+    }
+    //getters
+    public getHitmarks(){
+    for(: Boot boot);{
+        
+    }
+//geen idee
+    
+    }
+    
+  
+    
+}
