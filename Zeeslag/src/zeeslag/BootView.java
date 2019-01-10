510c88c2c6c260cd4710d29c4f82373998402c35
RensRemans <46380116+RensRemans@users.noreply.github.com>
2019-01-10 09:29:55 +0100

    Update BootView.java

--- Zeeslag/src/zeeslag/BootView.java
+++ Zeeslag/src/zeeslag/BootView.java
@@ -7,16 +7,35 @@ package zeeslag;
 
 import javafx.scene.layout.Region;
 import javafx.scene.image.ImageView;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Text;
 /**
  *
  * @author Bavo, Jonas, Rens
  */
 public class BootView extends Region {
     private Boot boot;
+    private Orientatie orientatie;
+    private int x,y;
     
     public BootView(){
         this.update();
     }
+
+    public BootView(Boot boot) {
+        this.boot = boot;
+        Rectangle achtergrond = new Rectangle(30,30);
+        achtergrond.setFill(Color.GRAY);
+        if (boot.HORIZONTAAL()) {
+            
+            
+        }
+        else
+        {
+            
+    }
+    
     
     public void update(){
         
