33497946c8b8e7969a8c467ecfc364c358c904e6
jonas2211 <jonas.iven@student.uhasselt.be>
2019-01-10 11:30:12 +0100

    nieuwe klasse ZeeslagSpel gemaakt

---
+++ Zeeslag/src/zeeslag/ZeeslagSpel.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package zeeslag;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author jonas
+ */
+public class ZeeslagSpel {
+    private ArrayList<Boot>boten;
+    
+    
+    public ZeeslagSpel(){
+        this.boten = new ArrayList<>();
+        boten.add(new Boot());
+    }
+
+    
+}
