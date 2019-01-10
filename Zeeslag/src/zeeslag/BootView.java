3ce56a86d9c94f11e4b3c9bb17eee489994b34a4
jonas2211 <jonas.iven@student.uhasselt.be>
2019-01-10 16:07:42 +0100

    Update BootView.java

--- Zeeslag/src/zeeslag/BootView.java
+++ Zeeslag/src/zeeslag/BootView.java
@@ -11,46 +11,40 @@ import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Text;
 import javafx.scene.transform.Rotate;
+
 /**
  *
  * @author Bavo, Jonas, Rens
  */
 public class BootView extends Region {
+
     private Boot boot;
     private Orientatie orientatie;
-    private int x,y;
-    
-    public BootView(){
+    private int x, y;
+
+    public BootView() {
         this.update();
     }
 
     public BootView(Boot boot) {
-        this.boot=boot;
-        
-        
+        this.boot = boot;
+
         if (boot.getOrientatie() == Orientatie.HORIZONTAAL) {
-            Rectangle achtergrond = new Rectangle(30*boot.getSize(),30);
+            Rectangle bootBody = new Rectangle(30 * boot.getSize(), 30);
+            bootBody.setFill(Color.GRAY);
+            getChildren().addAll(bootBody);
+        } else {
+
+            Rectangle achtergrond = new Rectangle(30, 30 * boot.getSize());
             achtergrond.setFill(Color.GRAY);
             getChildren().addAll(achtergrond);
         }
-        else
-        {
 
-            Rectangle achtergrond = new Rectangle(30,30*boot.getSize());
-            achtergrond.setFill(Color.GRAY);
-            getChildren().addAll(achtergrond);
-        }
-     
-    
-    
     }
-        
-    
 
-    
-    public void update(){
-        
+    public void update() {
+
     }
-    
+
 }
 //test
