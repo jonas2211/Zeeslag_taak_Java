28a5ae057be1d97962a61ae9190be9426f44eede
jonas2211 <jonas.iven@student.uhasselt.be>
2019-01-12 00:27:16 +0100

    Update Duikboot.java

--- Zeeslag/src/zeeslag/Duikboot.java
+++ Zeeslag/src/zeeslag/Duikboot.java
@@ -15,15 +15,18 @@ public class Duikboot extends Boot{
     private boolean gezonken;
     private Orientatie orientatie;
     private int rotatieHoek;
+    private int column;
+    private int row;
+    private Type type;
     
-    public Duikboot(String naam, int size, int x, int y, Orientatie o) {
-        super(naam, size, x, y, o);
+    public Duikboot(String naam, int size, int column, int row, Orientatie o, Type type) {
+        super(naam, column, row, o, type);
         naam = "onderzeeer";
         size = 3;
         gezonken = false;
         levens = size;
-        x = x;
-        y = y;
+        this.row = row;
+        this.column = column;
         orientatie = Orientatie.HORIZONTAAL;
         
     }
