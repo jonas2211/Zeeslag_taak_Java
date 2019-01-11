fef31e2b4d2c86ea62dd0d5b2786bc802aae2aad
jonas2211 <jonas.iven@student.uhasselt.be>
2019-01-12 00:27:06 +0100

    Create BordView.java

---
+++ Zeeslag/src/zeeslag/BordView.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package zeeslag;
+
+/**
+ *
+ * @author jonas
+ */
+public class BordView {
+    //getekendeBoot = new BootView(hier moet een boot-object komen);
+    
+}
