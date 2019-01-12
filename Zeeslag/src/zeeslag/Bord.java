3c0a6d46857645a696c73cb3d2a3387d61de82e8
RensRemans <46380116+RensRemans@users.noreply.github.com>
2019-01-12 02:24:17 +0100

    remove botenlist oud

--- Zeeslag/src/zeeslag/Bord.java
+++ Zeeslag/src/zeeslag/Bord.java
@@ -132,10 +132,8 @@ public class Bord {
     public Boot getGeselecteerdeBoot() {
         return geselecteerdeBoot;
     }
-   
     
-    
-    
+    //boolean methodes
     public boolean isBootGeselecteerd() {
         return (geselecteerdeBoot!= null);
     }
