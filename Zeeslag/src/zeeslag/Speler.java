03d0f3d6daa046dba051c16ec9e2813ceb53c0fe
BavoFranssen <bavo.franssen@student.uhasselt.be>
2019-01-12 15:49:26 +0100

    Create Speler.java

---
+++ Zeeslag/src/zeeslag/Speler.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package zeeslag;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Bavo
+ */
+public class Speler {
+    private Bord bord;
+    private int spelerLevens;
+    
+    
+    public Speler(Bord bord){
+        this.bord=bord;
+        //spelerLevens=
+    }
+    
+    //getters
+
+    public int getSpelerLevens() {
+        return spelerLevens;
+    }
+    
+    public ArrayList<Boot> getBoten(){
+        return bord.getBoten();
+        
+        
+        
+        
+        
+    }
+    
+    
+}
