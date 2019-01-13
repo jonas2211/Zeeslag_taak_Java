f5db3d863f80d0545f5ee5d672d529d547b716f9
BavoFranssen <bavo.franssen@student.uhasselt.be>
2019-01-13 22:39:57 +0100

    Update Boot.java

--- Zeeslag/src/zeeslag/Boot.java
+++ Zeeslag/src/zeeslag/Boot.java
@@ -130,15 +130,15 @@ public class Boot {
         }
     }
     
-    public void roteer(boolean roteerRechts){
+    public void roteer(){
         int rotatieHoek = 0;
 
-        if(roteerRechts && rotatieHoek == 0){
+        if(rotatieHoek == 0){
             rotatieHoek = 90;
         }
         else{
-            if(!roteerRechts && rotatieHoek == 90){
-                rotatieHoek = -90;
+            if(rotatieHoek == 90){
+                rotatieHoek = 0;
             }
         }
     }
