f0109576f849123b39cfd5ad2f69298bc0c639c1
jonas2211 <jonas.iven@student.uhasselt.be>
2019-01-16 00:39:36 +0100

    Update ZeeslagController.java

--- Zeeslag/src/zeeslag/ZeeslagController.java
+++ Zeeslag/src/zeeslag/ZeeslagController.java
@@ -90,6 +90,7 @@ public class ZeeslagController {
         int x = (int) mouseEvent.getX();
         int y = (int) mouseEvent.getY();
         
+        /*
         if(zeeslagModel.getHuidigeSpeler() == zeeslagModel.spelerEcht)  //is fout denk ik
         {
             boolean madeTurn = zeeslagModel.getBord().setHitmark(view.getBordModel().getRow(y), view.getBordModel().getColumn(x));
@@ -113,7 +114,7 @@ public class ZeeslagController {
                 zeeslagModel.eindeBeurt();
             }
         }
-     
+     */
         
     }
     
